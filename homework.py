3ce83c7443cffb7dcf1eead563f17ab4e1f167a6
Дмитрий Остапчук <wisphes@gmail.com>
2022-10-12 11:13:24 +0300

    commit

--- homework.py
+++ homework.py
@@ -126,7 +126,6 @@ def main():
             homeworks = check_response(response)
             message = parse_status(homeworks[0])
             send_message(bot, message)
-            current_timestamp = response.get('current_date')
         except NegativeSendMessageError as error:
             logger.error(error)
         except Exception as error:
